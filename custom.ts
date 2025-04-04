c4bf523e1f21a727333448c51c5f650d089429fa
Maciej Zientarski <m.zientarski@kezzler.com>
2025-04-04 23:24:28 +0200

    tilework #2

--- custom.ts
+++ custom.ts
@@ -45,6 +45,6 @@ namespace myCategory {
         i.fill(0)
         i.setPixel(0,1, 2)
         i.setPixel(1, 1, 2)
-        return tiles.createTilemap(b, i, [myTiles.transparency16, sprites.swamp.swampTile3, myTiles.t1000, myTiles.tile1, myTiles.tile1, myTiles.tile1, myTiles.tile1, myTiles.tile1], TileScale.Eight);
+        return tiles.createTilemap(b, i, [myTiles.transparency16, myTiles.t1000, sprites.swamp.swampTile3, myTiles.t1000, myTiles.tile1, myTiles.tile1, myTiles.tile1, myTiles.tile1, myTiles.tile1], TileScale.Eight);
     }
 }
