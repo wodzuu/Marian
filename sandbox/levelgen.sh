76e422f9e1d7a69ac84c048be98c32a756ca86e3
Maciej Zientarski <m.zientarski@kezzler.com>
2025-04-12 22:28:08 +0200

    namespace

--- sandbox/levelgen.sh
+++ sandbox/levelgen.sh
@@ -7,3 +7,5 @@ docker run \
     -w /work \
     node:22 \
     bash -c 'npm install -D ts-node && npx ts-node levelgen.ts'
+#    bash -c 'apt update && apt install -y cmake && npm install -g cmake-js && npm install -g static-script && ssc levelgen.ts'
+#    node levelgen.js
